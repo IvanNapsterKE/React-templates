39aa45c38a7ab166c91383067759e06aadf9f0f9
IvanNapsterKE <nguyoivan@gmail.com>
2023-04-10 16:08:55 +0300

    Added Clients Page

--- src/components/pages/Clients.jsx
+++ src/components/pages/Clients.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React from "react";
 
 function Clients() {
-    return (
-      <div>
-     <h2>clients</h2>
-      </div>
-    );
-  }
-  
-  export default Clients;
+  return (
+   
+    <section class="bg-white dark:bg-gray-900">
+    <div class="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
+        <h2 class="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">You’ll be in good company</h2>
+        <div class="grid grid-cols-2 gap-8 text-gray-500 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
+            <a href="#" class="flex justify-center items-center">
+            <img
+                  alt="John Doe"
+                  src="https://dummyimage.com/300"
+                  className="rounded-xl shadow-lg max-w-full h-auto align-middle border-none undefined"
+                />                          
+            </a>
+            <a href="#" class="flex justify-center items-center">
+            <img
+                  alt="John Doe"
+                  src="https://dummyimage.com/300"
+                  className="rounded-xl shadow-lg max-w-full h-auto align-middle border-none undefined"
+                />                                                 
+            </a>
+            <a href="#" class="flex justify-center items-center">
+            <img
+                  alt="John Doe"
+                  src="https://dummyimage.com/300"
+                  className="rounded-xl shadow-lg max-w-full h-auto align-middle border-none undefined"
+                />                                                                   
+            </a>
+
+            <a href="#" class="flex justify-center items-center">
+            <img
+                  alt="John Doe"
+                  src="https://dummyimage.com/300"
+                  className="rounded-xl shadow-lg max-w-full h-auto align-middle border-none undefined"
+                />                                                                                     
+            </a>
+            <a href="#" class="flex justify-center items-center">
+            <img
+                  alt="John Doe"
+                  src="https://dummyimage.com/300"
+                  className="rounded-xl shadow-lg max-w-full h-auto align-middle border-none undefined"
+                />                                                                                     
+            </a>
+            <a href="#" class="flex justify-center items-center">
+            <img
+                  alt="John Doe"
+                  src="https://dummyimage.com/300"
+                  className="rounded-xl shadow-lg max-w-full h-auto align-middle border-none undefined"
+                />                                                                                    
+            </a>
+        </div>
+    </div>
+</section>
+  );
+}
+
+export default Clients;
